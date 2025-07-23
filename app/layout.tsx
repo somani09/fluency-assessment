954057e1d24fee071a183d3df3511555f8a83097
Vaibhav Somani <vsomani2@gmail.com>
2025-07-23 13:47:44 -0500

    chore(darkmode): trying to remove dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body className={` ${spaceGrotesk.className} antialiased`}>
         <ClientRoot>{children}</ClientRoot>
       </body>
